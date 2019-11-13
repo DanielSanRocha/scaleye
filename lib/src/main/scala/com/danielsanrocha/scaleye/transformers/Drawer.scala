408605fb6d50d8e570131b55d78173438e52411a
daniel.rocha <daniel.rocha@corp.globo.com>
2019-11-13 19:07:21 -0300

    Added scaladocs for the Drawer object/class.

--- lib/src/main/scala/com/danielsanrocha/scaleye/transformers/Drawer.scala
+++ lib/src/main/scala/com/danielsanrocha/scaleye/transformers/Drawer.scala
@@ -4,18 +4,38 @@ import com.danielsanrocha.scaleye.Color
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import com.danielsanrocha.scaleye.linearalgebra._
+import com.danielsanrocha.scaleye.linearalgebra.{Matrix, Point, Line}
 import org.bytedeco.javacpp.opencv_imgproc
 
+/** Draw simple geometric forms in a image. **/
 object Drawer {
+
+  /** Draw a line in a image.
+    * @param img - Image to draw; this method will alter the original image.
+    * @param line - Object that holds the position of the line that will be draw, for more information see [[linearalgebra.Line]].
+    * @param color - The color of the line, for more information see [[Color]].
+    * @param thickness - The thickness of the line as a integer.
+    * **/
   def apply(img: Matrix, line: Line, color: Color, thickness: Int): Future[Unit] =
     new LineDrawer(line, color, thickness)(img)
+
+  /** Draw a point (filled circle) in a image.
+    * @param img - Image to draw; this method will alter the original image.
+    * @param point - Object that holds the position of the point that will be draw, for more information see [[linearalgebra.Point]].
+    * @param color - The color of the point, for more information see [[Color]].
+    * @param radius - The radius of the point as a integer.
+    * **/
   def apply(img: Matrix, point: Point, color: Color, radius: Int): Future[Unit] =
     new PointDrawer(point, color, radius)(img)
 }
 
-trait Drawer extends Transformer {}
+private[transformers] trait Drawer extends Transformer {}
 
+/** Create a transformer that draw a line into images.
+  * @param line - Object that holds the position of the line that will be draw, for more information see [[linearalgebra.Line]].
+  * @param color - The color of the line, for more information see [[Color]].
+  * @param thickness - The thickness of the line as a integer.
+  * **/
 class LineDrawer(line: Line, color: Color, thickness: Int) extends Drawer {
   override def apply(img: Matrix): Future[Unit] = {
     Future {
@@ -26,6 +46,11 @@ class LineDrawer(line: Line, color: Color, thickness: Int) extends Drawer {
   }
 }
 
+/** Draw a transformer that draw a point into images.
+  * @param point - Object that holds the position of the point that will be draw, for more information see [[linearalgebra.Point]].
+  * @param color - The color of the point, for more information see [[Color]].
+  * @param radius - The radius of the point as a integer.
+  * **/
 class PointDrawer(point: Point, color: Color, radius: Int) extends Drawer {
   override def apply(img: Matrix): Future[Unit] = {
     Future {
